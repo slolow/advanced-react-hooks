eb910bb401b28a0c0d59895118c6ade54d5dd265
Laszlo Starost <laszlo.starost@barmer.de>
2024-03-08 13:00:43 +0100

    exercise-1-extra-credit-4

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -4,19 +4,20 @@
 import * as React from 'react'
 
 const countReducer = (state, action) => {
-  return {
-    ...state,
-    ...(typeof action === 'function' ? action(state) : action)
+  switch (action.type) {
+    case 'INCREMENT':
+      return {count: state.count +action.step};
+    default:
+        throw Error(`dispatch type not suported: ${action.type}`);
   }
 }
 
 function Counter({initialCount = 0, step = 1}) {
-  const [state, setState] = React.useReducer(countReducer, {
+  const [state, dispatch] = React.useReducer(countReducer, {
     count: initialCount,
   })
   const {count} = state
-  const increment = () =>
-      setState(currentState => ({count: currentState.count + step}))
+  const increment = () => dispatch({type: 'INCREMENT', step})
   return <button onClick={increment}>{count}</button>
 }
 
